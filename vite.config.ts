c7c270d6374adcdfa624bc4fae48a739caddcc17
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-10 15:59:46 +0300

    THB#72: add temp proxy, fix auth api urls

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,22 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	]
+  plugins: [
+    sveltekit(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide'
+    })
+  ],
+
+  // temp dev proxy
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
