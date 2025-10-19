b38c59e62a7804b0714e70acd416c5a03e0285df
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-19 22:49:47 +0300

    THB#105: init shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,13 @@
-/**
- * Helper to explicitly mark a promise as deferred for SvelteKit load functions.
- * This prevents TypeScript from auto-unwrapping the Promise type in PageData.
- */
-export function defer<T>(promise: Promise<T>): Promise<T> {
-  return promise;
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
