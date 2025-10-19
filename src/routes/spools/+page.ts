470ed1dd25b2790f601b5975fe7a3d409a13de63
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-19 19:00:54 +0300

    Fix spool list not loading

--- src/routes/spools/+page.ts
+++ src/routes/spools/+page.ts
@@ -2,7 +2,9 @@ import type { PageLoad } from './$types';
 import { SpoolApi } from '$lib/api/spool';
 
 export const load: PageLoad = async () => {
+  const spoolDockProps = await SpoolApi.getUserSpoolList();
+
   return {
-    spools: await SpoolApi.getUserSpoolList()
+    spools: spoolDockProps.spools
   };
 };
