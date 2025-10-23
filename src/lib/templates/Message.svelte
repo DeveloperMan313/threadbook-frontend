fa8aabfb4a8090b7ac180e88f929edf67bcbb2d7
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-23 21:53:31 +0000

    THB#120: add message component

---
+++ src/lib/templates/Message.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { MessageProps } from '$lib/types';
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
+
+  const { username, userPfp, text, createdAt }: MessageProps = $props();
+</script>
+
+<div class="w-full flex">
+  <div class="w-[5rem] flex-none">
+    <Avatar.Root class="size-[3rem]">
+      <Avatar.Image src={userPfp} alt="@{username}" />
+      <Avatar.Fallback>{username.slice(0, 2).toUpperCase()}</Avatar.Fallback>
+    </Avatar.Root>
+  </div>
+  <div class="flex-1">
+    <div class="w-full">
+      <h4 class="inline me-2 scroll-m-20 text-xl font-semibold tracking-tight">{username}</h4>
+      <p class="inline text-sm text-muted-foreground">{createdAt.toLocaleTimeString()}</p>
+    </div>
+    <p class="w-full">{text}</p>
+  </div>
+</div>
