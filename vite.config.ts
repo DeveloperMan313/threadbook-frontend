57bbd2d22a32089d28b5ed35bac33c4db78c5974
DeveloperMan313 <053000voloshch@gmail.com>
2025-10-24 18:12:45 +0300

    Add api proxy to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
 import tailwindcss from '@tailwindcss/vite';
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    sveltekit(),
-    paraglideVitePlugin({
-      project: './project.inlang',
-      outdir: './src/lib/paraglide'
-    })
-  ],
-  build: {
-    rollupOptions: {
-      external: ['deepfilternet3-noise-filter']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      tailwindcss(),
+      sveltekit(),
+      paraglideVitePlugin({
+        project: './project.inlang',
+        outdir: './src/lib/paraglide'
+      })
+    ],
+    build: {
+      rollupOptions: {
+        external: ['deepfilternet3-noise-filter']
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY_API_ORIGIN,
+          changeOrigin: true,
+          secure: true
+        }
+      }
     }
-  }
+  };
 });
