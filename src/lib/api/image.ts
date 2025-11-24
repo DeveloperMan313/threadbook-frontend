a22db2e3cc1dbbee823dae93bba16ff699381e06
DeveloperMan313 <053000voloshch@gmail.com>
2025-11-24 15:57:17 +0300

    Fix requesting nonexistent images

--- src/lib/api/image.ts
+++ src/lib/api/image.ts
@@ -4,28 +4,28 @@ import { PUBLIC_API_ORIGIN } from '$env/static/public';
  * Get image URL
  * @param {string} bucket - image bucket
  * @param {string} filename - image filename
- * @returns {string} - image URL
+ * @returns {string?} - image URL or undefined if filename is empty
  */
-const getImageURL = (bucket: string, filename: string): string => {
-  return `${PUBLIC_API_ORIGIN}/uploads/${bucket}/${filename}`;
+const getImageURL = (bucket: string, filename: string): string | undefined => {
+  return filename ? `${PUBLIC_API_ORIGIN}/uploads/${bucket}/${filename}` : undefined;
 };
 
 export const ImageApi = {
   /**
    * Get user avatar URL
    * @param {string} filename - avatar filename
-   * @returns {string} - avatar URL
+   * @returns {string?} - avatar URL or undefined if filename is empty
    */
-  getUserAvatarURL(filename: string): string {
+  getUserAvatarURL(filename: string): string | undefined {
     return getImageURL('avatars', filename);
   },
 
   /**
    * Get spool banner URL
    * @param {string} filename - banner filename
-   * @returns {string} - banner URL
+   * @returns {string?} - banner URL or undefined if filename is empty
    */
-  getSpoolBannerURL(filename: string): string {
+  getSpoolBannerURL(filename: string): string | undefined {
     return getImageURL('spools', filename);
   }
 };
