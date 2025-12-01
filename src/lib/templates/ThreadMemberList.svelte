174c49696cc4fc8c055255f5521933416696a91b
DeveloperMan313 <053000voloshch@gmail.com>
2025-12-01 23:39:20 +0300

    Add member list loading skeletons

--- src/lib/templates/ThreadMemberList.svelte
+++ src/lib/templates/ThreadMemberList.svelte
@@ -3,6 +3,7 @@
   import { getContext } from 'svelte';
   import UserBar from './UserBar.svelte';
   import { SpoolApi, ThreadApi } from '$lib/api';
+  import { Skeleton } from '$lib/components/ui/skeleton';
 
   const { spoolId }: ThreadMemberListProps = $props();
 
@@ -16,6 +17,7 @@
   };
 
   let usernames = $state<string[]>([]);
+  let isLoading = $state(false);
 
   const mode = $derived.by(() => {
     const threadId = getCurrentThreadId();
@@ -28,11 +30,13 @@
   $effect(() => {
     // TEMP need to move threads to global state and cache them
     const threadId = getCurrentThreadId(); // TEMP capture
+    isLoading = true;
     if (mode === 'private') {
       ThreadApi.getMembers({ thread_id: threadId! }).then((members) => {
         if (getCurrentThreadId() !== threadId) return; // TEMP
         usernames = members.map((m) => m.username);
         cacheProfilesFromUsernames(usernames);
+        isLoading = false;
       });
       return;
     }
@@ -40,6 +44,7 @@
       if (getCurrentThreadId() !== threadId) return; // TEMP
       usernames = response.members.map((m) => m.username);
       cacheProfilesFromUsernames(usernames);
+      isLoading = false;
     });
   });
 </script>
@@ -47,8 +52,20 @@
 <div class="flex h-full w-full flex-col bg-primary-foreground px-4 py-5">
   <h3 class="pb-2 text-2xl">{mode === 'private' ? 'Thread members' : 'Spool members'}</h3>
   <div class="w-full flex-1 space-y-2 overflow-y-auto pl-2">
-    {#each usernames as username (username)}
-      <UserBar {username} />
-    {/each}
+    {#if isLoading}
+      {#each { length: 3 }}
+        <div class="flex items-center space-x-4">
+          <Skeleton class="size-12 flex-none rounded-full" />
+          <div class="w-full space-y-2">
+            <Skeleton class="h-4 w-32" />
+            <Skeleton class="h-4 w-24" />
+          </div>
+        </div>
+      {/each}
+    {:else}
+      {#each usernames as username (username)}
+        <UserBar {username} />
+      {/each}
+    {/if}
   </div>
 </div>
